68b7c3ae4c03dfb9431091ee777fbb898d4049a8
ooeygooeygold <amichaelj1@gmail.com>
2018-10-15 16:30:48 -0400

    Add Router and Items

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import './App.css'
 import ItemList from './ItemList'
 import Item from './Item'
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <main>
-        <h1>iShop</h1>
-        <div className="tabs is-centered is-boxed is-medium">
-          <ul>
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Best seller</a>
-            </li>
-            <li>
-              <a>Featured</a>
-            </li>
-            <li>
-              <a>All Items</a>
-            </li>
-          </ul>
-        </div>
-        <ItemList />
-        <Item />
-      </main>
+      <Router>
+        <main>
+          <h1>The Fitness Shop</h1>
+          <div className="tabs is-centered is-boxed is-medium">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/BestSellers">Best Sellers</Link>
+              </li>
+              <li>
+                <Link to="/Featured">Featured</Link>
+              </li>
+              <li>
+                <Link to="/All Items">All Items</Link>
+              </li>
+            </ul>
+          </div>
+          <ItemList />
+          <Item />
+        </main>
+      </Router>
     )
   }
 }
--- src/Item.js
+++ src/Item.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 class Item extends Component {
   state = {}
   render() {
     return (
       <>
-        <h1>Battery Pack iPhone 8</h1>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51GimrbsYAL._SX522_.jpg" />
+        <h1>Gold Standard Protien</h1>
+        <img src="https://www.gnc.com/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog-gnc/default/dw33b26f87/hi-res/350280_1.jpg?sw=350&sh=350&sm=fit" />
         <p>price</p>
-        <button>Buy</button>
+        <Route
+          render={({ history }) => (
+            <button
+              type="button"
+              onClick={() => {
+                history.push('/BestSeller')
+              }}
+            >
+              Buy!
+            </button>
+          )}
+        />
       </>
     )
   }
